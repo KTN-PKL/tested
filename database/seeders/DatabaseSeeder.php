db0b5454a14ec49dadc670b63c3659cd0bba3ae3
bagaas86 <bagaas86@gmail.com>
2023-02-01 11:17:00 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,9 @@ class DatabaseSeeder extends Seeder
             'name' => 'admin',
             'email' => 'admin@admin.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
+            'level' => 'admin',
+            'alamat'=>'Desa X',
+            'no_telp'=>'0822',
             
             
         ]);

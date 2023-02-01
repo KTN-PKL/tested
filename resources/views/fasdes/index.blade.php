872f66db0fd3322b3572d2e2800c6c8385c3f933
bagaas86 <bagaas86@gmail.com>
2023-02-01 11:43:40 +0700

    route web

--- resources/views/fasdes/index.blade.php
+++ resources/views/fasdes/index.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.template')
 @section('content')
 <div class="pagetitle">
-    <h1>Dashboard</h1>
+    <h1>Daftar Fasilitator Desa</h1>
     <nav>
       <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="{{url('dashboard')}}">Dashboard</a></li>
+        <li class="breadcrumb-item active">Fasilitator Desa</li>
       </ol>
     </nav>
   </div><!-- End Page Title -->
 
-
+  
 Ini Halaman Dashboard
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\c_login;
+use App\Http\Controllers\c_fasdes;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +15,21 @@ use App\Http\Controllers\c_login;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
+Route::get('/test', function () {
+    return view('fasdes.index');
 });
 
 Route::get('/', [App\Http\Controllers\c_login::class, 'index'])->name('login');
 Route::get('/dashboard', [App\Http\Controllers\c_login::class, 'dashboard'] )->name('dashboard')->middleware('auth');
 Route::get('/check', [App\Http\Controllers\c_login::class, 'check'])->name('login.check');
 Route::post('/', [App\Http\Controllers\c_login::class, 'logout'])->name('user.logout');
+
+
+Route::controller(c_fasdes::class)->middleware('auth')->group(function () {
+    Route::get('fasdes', 'index')->name('fasdes');
+    Route::post('fasdes/store', 'store')->name('fasdes.store');
+    Route::get('fasdes/create', 'create')->name('fasdes.create');
+    Route::get('fasdes/edit/{id}', 'edit')->name('fasdes.edit');
+    Route::post('fasdes/update/{id}', 'update')->name('fasdes.update');
+    Route::get('fasdes/destroy/{id}', 'destroy')->name('fasdes.destroy');
+});

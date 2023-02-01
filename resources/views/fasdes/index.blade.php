4db912ff80c675568046533960277dbf92f4c653
bagaas86 <bagaas86@gmail.com>
2023-02-01 12:49:47 +0700

    create fasdes

--- resources/views/fasdes/index.blade.php
+++ resources/views/fasdes/index.blade.php
@@ -12,7 +12,7 @@
   <div class="card">
     <div class="card-body">
       <div class="col mt-4">
-        <a href="#" class="btn btn-primary">Create Fasilitator Desa</a>
+        <a href="{{route('fasdes.create')}}" class="btn btn-primary">Create Fasilitator Desa</a>
       </div>
       <br>
       <div>
@@ -22,14 +22,28 @@
               <th>No</th>
               <th>Email</th>
               <th>Nama Fasilitator Desa</th>
-              <th>Alamat</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
+            @php
+            $i=0;
+            @endphp
+            @foreach ($fasdes as $data)
+            @php
+            $i=$i+1;
+            @endphp
             <tr>
-              <td></td>
+              <td>{{$i}}</td>
+              <td>{{$data->email}}</td>
+              <td>{{$data->name}}</td>
+              <td style="width:25%">
+                <a href="" class="btn btn-sm btn-primary"> <i class="bi bi-eye"></i> Lihat</a>
+                <a href="" class="btn btn-sm btn-warning"><i class="bi bi-pencil-square"></i> Edit</a>
+                <a href="" class="btn btn-sm btn-danger"><i class="bi bi-trash"></i> Hapus</a>
+              </td>
             </tr>
+            @endforeach
           </tbody>
         </table>
       </div>

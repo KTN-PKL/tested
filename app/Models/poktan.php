154e3b4e13512de46f239bedfa11a78ef375ff78
gusnanda7 <gus11111234@gmail.com>
2023-02-01 12:47:35 +0700

    membuat contoler dan model poktan

--- app/Models/poktan.php
+++ app/Models/poktan.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class poktan extends Model
 {
     use HasFactory;
+
+    public function fasdesData($id)
+    {
+        DB::table('poktans')->where('id_user', $id)->get();
+    }
+    public function addData($data)
+    {
+        DB::table('poktans')->insert($data);
+    }
+    public function detailData($id)
+    {
+        return DB::table('poktans')->where('id_poktan', $id)->first();
+    }
+    public function editData($id, $data)
+    {
+        return DB::table('poktans')->where('id_poktan', $id)->update($data);
+    }
+    public function deleteData($id)
+    {
+        return DB::table('poktans')->where('id_poktan', $id)->delete();
+    }
 }

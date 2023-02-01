9265c4f8f2f623faa9242efe521e21e3cf373ec3
bagaas86 <bagaas86@gmail.com>
2023-02-01 13:06:54 +0700

    edit fasdes

--- resources/views/fasdes/edit.blade.php
+++ resources/views/fasdes/edit.blade.php
@@ -18,19 +18,41 @@
         
 
               <!-- Vertical Form -->
-              <form class="row g-3" action="{{route('faskab.fasdes.store')}}" method="POST">
+              <form class="row g-3" action="{{route('faskab.fasdes.update', $fasdes->id)}}" method="POST">
                 @csrf
                 <div class="col-12">
                   <label for="inputNanme4" class="form-label">Nama Fasilitator Desa</label>
-                  <input type="text" class="form-control" name="name">
+                  <input type="text" class="form-control" name="name" value="{{$fasdes->name}}">
                 </div>
                 <div class="col-12">
                   <label for="inputEmail4" class="form-label">Email</label>
-                  <input type="email" class="form-control" name="email">
+                  <input type="email" class="form-control" name="email" value="{{$fasdes->email}}">
                 </div>
                 <div class="col-12">
-                  <label for="inputPassword4" class="form-label">Password</label>
+                  <label for="inputPassword4" class="form-label">Password</label> <i style="font-size:12px" class="text-muted">Masukkan Password Baru</i>
                   <input type="password" class="form-control" name="password">
+                 
+                </div>
+                <div class="col-12">
+                  <label for="inputPassword4" class="form-label">Alamat</label>
+                  <input type="text" class="form-control" name="alamat" value="{{$fasdes->alamat}}">
+                </div>
+                <div class="col-12">
+                  <label for="inputPassword4" class="form-label">Nomor Telepon</label>
+                  <input type="text" class="form-control" name="notelp" value="{{$fasdes->no_telp}}">
+                </div>
+                <div class="col-12">
+                  <label for="inputPassword4" class="form-label">Alamat</label>
+                  <select class="form-select" name="jeniskelamin">
+                    <option value="">-- Pilih Jenis Kelamin --</option>
+                    <option value="Pria" @if ($fasdes->jeniskelamin == "Pria")
+                      selected
+                    @endif>Pria</option>
+                    <option value="Wanita" @if ($fasdes->jeniskelamin == "Wanita")
+                      selected
+                    @endif>Wanita</option>
+                    
+                  </select>
                 </div>
                 <div class="text-center">
                   <button type="submit" class="btn btn-primary">Submit</button>

afaaeb8e324add7c815f3d6cf2f8f2af6119c17c
bagaas86 <bagaas86@gmail.com>
2023-02-03 14:18:36 +0700

    view kegiatan

--- resources/views/user/absen/kegiatan.blade.php
+++ resources/views/user/absen/kegiatan.blade.php
@@ -42,6 +42,9 @@
             <a href="#" class="btn btn-block btn-warning">
               <i class="fa-solid fa-user"></i> Foto Selfie</a
             >
+              <div>
+         
+        </div>
         </div>
 
         <!-- field deskripsi -->
@@ -151,9 +154,30 @@
   </script>
   {{-- end form pelatihan --}}
 
-  {{-- geolokasi --}}
- 
-  {{-- end geolokasi --}}
+  <script type="text/javascript">
+    // seleksi elemen video
+    var video = document.querySelector("#video-webcam");
+
+    // minta izin user
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+    // jika user memberikan izin
+    if (navigator.getUserMedia) {
+        // jalankan fungsi handleVideo, dan videoError jika izin ditolak
+        navigator.getUserMedia({ video: true }, handleVideo, videoError);
+    }
+
+    // fungsi ini akan dieksekusi jika  izin telah diberikan
+    function handleVideo(stream) {
+        video.srcObject = stream;
+    }
+
+    // fungsi ini akan dieksekusi kalau user menolak izin
+    function videoError(e) {
+        // do something
+        alert("Izinkan menggunakan webcam untuk demo!")
+    }
+</script>
  
 </body>
 @endsection

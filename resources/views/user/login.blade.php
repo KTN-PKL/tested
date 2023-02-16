2bf253e78674550a713095cd5036d410aed9a5f8
febriananur <febriananurokhman@gmail.com>
2023-02-16 16:06:35 +0700

    login sweet alert

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -12,7 +12,6 @@
   <body class="css-selector text-light">
     <div class="container p-5">
        @include('sweetalert::alert')
-     
       <div class="icon text-center p-5"><i class="fa-solid fa-id-card-clip fa-5x text-light"></i></div>
       <h1>Selamat Datang Fasdes</h1>
       <form action="{{route('login.check')}}">

a7f64edd9e41577f5fa9a9dfe2a487e4cddc02ad
bagaas86 <bagaas86@gmail.com>
2023-02-25 15:30:36 +0700

    commit

--- resources/views/webgis.blade.php
+++ resources/views/webgis.blade.php
@@ -5,8 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="http://unpkg.com/leaflet@1.4.0/dist/leaflet.css" />
-    <script src="http://unpkg.com/leaflet@1.4.0/dist/leaflet.js"></script>
+    {{-- <link rel="stylesheet" href="http://unpkg.com/leaflet@1.4.0/dist/leaflet.css" />
+    <script src="http://unpkg.com/leaflet@1.4.0/dist/leaflet.js"></script> --}}
+    <!-- Leaflet CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.css" />
+
+<!-- Leaflet JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.js"></script>
+
+<script src='https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-omnivore/v0.3.1/leaflet-omnivore.min.js'></script>
+
     <script src="{{asset('templateUser')}}/kml/kml.js"></script>
 </head>
 <style>
@@ -15,6 +23,7 @@
 <body>
     
     <div id="map"></div>
+    <div id="sidebar"></div>
     <script>
         var map = L.map('map').setView([-6.560261,107.763734], 13);
         const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -22,23 +31,27 @@
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
 
+ 
 	
 
-    // Load kml file
+   
   
-    fetch('public/test.kml')
-                .then(res => res.text())
-                .then(kmltext => {
-                    // Create new kml overlay
-                    const parser = new DOMParser();
-                    const kml = parser.parseFromString(kmltext, 'text/xml');
-                    const track = new L.KML(kml);
-                    map.addLayer(track);
+   // menambahkan layer dari KML
+    var kmlLayer = omnivore.kml('path/to/kml').addTo(map)
 
-                    // Adjust map to show the kml
-                    const bounds = track.getBounds();
-                    map.fitBounds(bounds);
-                });
+   // Bind popup to each feature on click
+   kmlLayer.on('click', function(e) {
+        var name = e.layer.feature.properties.name;
+        var description = e.layer.feature.properties.description;
+        var coordinates = e.layer.feature.geometry.coordinates;
+
+    // Do something with the image data (e.g. display the image)
+
+        var popupContent = '<h3>' + name + '</h3>' +
+                           '<div>' + description + '</div>';
+                                              
+        e.layer.bindPopup(popupContent).openPopup();
+    });
     </script>
 </body>
 </html>
